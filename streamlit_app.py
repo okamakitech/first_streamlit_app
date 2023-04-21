692b446f536f191d4cf157ae3f11e28d2368c6d3
okamakitech <131335032+okamakitech@users.noreply.github.com>
2023-04-21 10:05:20 +0900

    Update streamlit_app.py
    Fix Header
    Add emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Manu')
-streamlit.text('Omega 3 & Bulueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🥣 Breakfast Favorites')
+streamlit.text('🥗 Omega 3 & Bulueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
